44031771e26b8b235f219571d9a0b5d046f5961a
Tymon Jedliński <tymon.jed.sub@gmail.com>
2026-03-15 02:32:06 +0100

    testy i skrypt do ich tworzenia

---
+++ gen_test.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+
+trap "echo 'Ctrl+C pressed, killing all background jobs'; kill 0; exit" SIGINT
+
+rm -rf small medium large
+
+ghc gen
+
+mkdir -p small
+for((i=0;i<1000;++i));do
+#for((i=0;i<10;++i));do
+    echo "10 $i 0.45 0.08" | ./gen  > small/$i.in &
+done
+
+mkdir -p medium
+for((i=0+2137;i<1000+2137;++i));do
+    echo "10 $i 0.8 0.05" | ./gen  > medium/$i.in &
+done
+
+mkdir -p large
+for((i=10000;i<10010;++i));do
+    echo "10 $i 0.6 0.01" | ./gen  > large/$i.in &
+done
+
+wait
+
+cat small/*  | wc
+cat medium/* | wc
+cat large/*  | wc
+
+for size in small medium large; do
+    read total_lines total_words total_bytes <<< $(cat "$size"/* | wc)
+    avg_bytes=$(( total_bytes / total_lines * 11 / 10))
+    max_bytes=$(cat $size/* | wc -L)
+    echo "in $size average bytes per tree: $avg_bytes, max was: $max_bytes"
+done
+
+echo "compiling program to generate answers"
+
+./run_tests.sh "just compile" || exit 1
+ans_prog=check.hs
+
+echo "now running toster to generate the tests, good luck!"
+
+echo "./${ans_prog%.hs}"
+
+toster --io small  ./${ans_prog%.hs} --generate
+toster --io medium ./${ans_prog%.hs} --generate
+toster --io large  ./${ans_prog%.hs} --generate --timeout 10000
+exit
+for f in large/*.in; do
+    echo $f ${f%.in}.out
+    time ./${ans_prog%.hs} < $f > ${f%.in}.out
+done
---
+++ run_tests.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+
+#program containing printPath function
+lib="main.hs"
+
+#temporary program that holds the reading of the trees
+checker="check.hs"
+
+cp $lib $checker
+
+sed -i '/main/,$d' $checker
+
+
+cat <<EOF >> $checker
+--code by chatgpt, i won't pretend it was me
+readNExprs :: Int -> IO [Expr]
+readNExprs 0 = return []
+readNExprs m = do
+    line <- getLine               -- read one line
+    let parsed = read line :: Expr
+    rest <- readNExprs (m - 1)    -- read the remaining lines
+    return (parsed : rest)
+
+-- ----------------------------------------------------------------------
+-- 2. Main routine
+-- ----------------------------------------------------------------------
+main :: IO ()
+main = do
+    -- 1  read how many trees we will get
+    nLine <- getLine
+    let n :: Int
+        n = read nLine
+
+    -- 2  read *n* trees
+    exprs <- readNExprs n
+
+    -- 3  process each one (here we just pretty‑print it)
+    --       (this is a small hand‑rolled mapM_)
+    let loop [] = return ()
+        loop (e:es) = do
+            printPath e
+            loop es
+
+    loop exprs
+
+EOF
+
+ghc $checker
+
+if (($# != 0)); then
+    echo "this is just compiling the checker for gen_test"
+    exit 0
+fi
+
+
+./${checker%.hs}
